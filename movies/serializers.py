195478e744233321e6c8165043b0d29c38c297e7
LucasRib52 <97406660+LucasRib52@users.noreply.github.com>
2024-07-14 00:38:43 -0300

    first commit

---
+++ movies/serializers.py
@@ -0,0 +1,39 @@
+from django.db.models import Avg
+from rest_framework import serializers
+from genres.serializers import GenreSerializer
+from actors.serializers import ActorSerializer
+from movies.models import Movie
+
+
+class MovieModelSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+    def validate_release_date(self, value):
+        if value.year < 1900:
+            raise serializers.ValidationError('A data de lancamento nao pode ser anterior a 1990.')
+        return value
+
+    def validate_resume(self, value):
+        if len(value) > 500:
+            raise serializers.ValidationError('Resumo nao deve ser maior que 200 caracteres.')
+        return value
+    
+
+class MovieListDetailSerializer(serializers.ModelSerializer):
+    actors = ActorSerializer(many=True)
+    genre = GenreSerializer()
+    rate = serializers.SerializerMethodField(read_only=True)
+    
+    class Meta:
+        model = Movie
+        fields = ['id', 'title', 'genre', 'actors', 'release_date', 'rate', 'resume']
+
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+
+        if rate:
+            return round(rate, 1)
+        return None
